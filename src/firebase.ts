625c00643dbdfe67a6ef7af5c1ad9d0722ac0272
felipesantos-ss <felipesantoss427@gmail.com>
2026-02-16 14:37:23 -0300

    firebase configuration

---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth/web-extension";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDs5ecYTiVVOwHwNfyu5nSWHCUJ8SS55AU",
+    authDomain: "chathub-76512.firebaseapp.com",
+    databaseURL: "https://chathub-76512-default-rtdb.firebaseio.com",
+    projectId: "chathub-76512",
+    storageBucket: "chathub-76512.firebasestorage.app",
+    messagingSenderId: "724912275673",
+    appId: "1:724912275673:web:3bd199736dcb02976b7ec8",
+    measurementId: "G-FZ8FJE5TT8"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export const db = getDatabase(app);
